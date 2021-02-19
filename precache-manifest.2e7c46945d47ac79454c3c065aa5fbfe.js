16a212582295914c14a61e39e8dbe4fe3f4b49b0
majd eddine ben tahar <majdeddinebentahar@gmail.com>
2021-02-19 09:11:52 +0100

    Updates

---
+++ precache-manifest.2e7c46945d47ac79454c3c065aa5fbfe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a17157ed5637806f50860fc77d76c96",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "68eb8820ebc4619b46e6",
+    "url": "/github-users/static/css/main.c03de443.chunk.css"
+  },
+  {
+    "revision": "a7bef6f2b85611a924fa",
+    "url": "/github-users/static/js/2.a0078ca4.chunk.js"
+  },
+  {
+    "revision": "606387e1ffc370f8ee861ca0fb1cbdb9",
+    "url": "/github-users/static/js/2.a0078ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68eb8820ebc4619b46e6",
+    "url": "/github-users/static/js/main.3450370a.chunk.js"
+  },
+  {
+    "revision": "2d7c28a818598e97ecea",
+    "url": "/github-users/static/js/runtime-main.bcf65e47.js"
+  },
+  {
+    "revision": "37b1590da17ec65adfa29196e4930190",
+    "url": "/github-users/static/media/follower.37b1590d.svg"
+  },
+  {
+    "revision": "a8ddaba2cc3442c3af59d987414b59ff",
+    "url": "/github-users/static/media/followers.a8ddaba2.svg"
+  },
+  {
+    "revision": "e5a607516164f9664df1df1165916907",
+    "url": "/github-users/static/media/link.e5a60751.svg"
+  },
+  {
+    "revision": "a4737ab7b5c0879b8fed1d581b8416d8",
+    "url": "/github-users/static/media/location.a4737ab7.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/github-users/static/media/loupe.ecef733a.svg"
+  },
+  {
+    "revision": "49b31114f36b647673de3949debf12b6",
+    "url": "/github-users/static/media/notebook.49b31114.svg"
+  }
+]);
